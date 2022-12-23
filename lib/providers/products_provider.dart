8dfd26f51b4e7c9809308e29b109ff14f3596994
Ezinne Nnabugwu <ezinne.blessing@yahoo.com>
2022-12-23 12:34:58 +0100

    patch request

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -123,9 +123,19 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      Uri url = Uri.parse(
+          'https://flutter-learning-68887-default-rtdb.firebaseio.com/products/$id.json');
+      await http.patch(url,
+          body: json.encode({
+            'title': newProduct.title,
+            'price': newProduct.price,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+          }));
+
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
